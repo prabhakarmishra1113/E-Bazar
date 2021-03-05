4127bf8699c6744c045e84d6737b2c86a2f4324c
PD1113 <prabhakarmishra1113@gmail.com>
2021-03-06 01:05:46 +0530

    product_page design is going on

--- templates/views/products_page.php
+++ templates/views/products_page.php
@@ -2,38 +2,82 @@
 
 <?php include "../includes/navbar.php" ?>
 
- <div class="container-fluid">
+<!--Product Page Start-->  
+ <div class="container-fluid product_page">
     <div class="row">
-       <div class="col-3">
+       <div class="col-lg-2 filter hide-on-small">
+         
+         <div class="card">
+            <div class="card-header">Filter</div>
+            <div class="card body">
+              <h5 class="card-title">Short By</h5>
+              <ul>
+                <a href="#"><li>Price low to high</li></a>
+                <a href="#"><li>Price high to low</li></a>
+                <a href="#"><li>Popularity</li></a>
+                <a href="#"><li>Newest</li></a>
+              </ul>
+            </div>
+         </div>
 
-         <div class="row">
-
-          <div class="col-4">
-             <div class="list-group" id="list-tab" role="tablist">
-               <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-               <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
-               <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
-               <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
-             </div>
-           </div>
-           
-           <div class="col-8">
-             <div class="tab-content" id="nav-tabContent">
-               <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
-               <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-               <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-               <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-             </div>
-           </div>
-
-          </div>
         </div>
 
-       <div class="col-9">
-       
+     <!--Product Section Start-->    
+       <div class="col-12 col-lg-10 product_sec">      
+         <div class="card">
+           <div class="card-header">Men's T-shirt</div>
+           <div class="card-body">
+              
+             <!--Product Card Start-->
+             <div class="row">
+             <?php
+               $i=0;
+               while($i<=10){
+               $i++;  
+             ?>
+              <div class="col-6 col-md-4 col-lg-3">
+               <div class="card">
+                 <a class="text-center" href="#"><img src="../../public/images/men/m1.jpeg" class="img-fluid"></a>
+                 <div class="card-footer">
+                   <figcaption class="figure-caption brand">E-BAZAR</figcaption>
+                   <a href="#"><figcaption class="figure-caption title">Men Branded T-shirt</figcaption></a>
+                   <figcaption class="figure-caption color">blue,red,pink</figcaption>
+                   <figcaption class="figure-caption rs">₹629 <del>₹999</del> <span class="text-success">76% off</span></figcaption>
+                   <button class="btn btn-primary w-100 mt-1">Add To Card</button>
+                 </div>
+               </div>
+              </div>
+            <?php
+              }
+            ?>
+            </div>
+             <!--Product Card End-->         
+           </div>
+
+           <!--Pagination Start-->
+            <nav aria-label="Page navigation example">
+               <ul class="pagination justify-content-center">
+                 <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                 </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                 <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                 </li>
+                </ul>
+            </nav>
+            <!--Pagination End-->
+         </div>     
        </div>
-
+    <!--Product Section End-->
     </div>
+  <!--Product Page Row End-->  
  </div>
-
+<!--Product Page End-->  
 <?php include "../includes/footer.php" ?>
