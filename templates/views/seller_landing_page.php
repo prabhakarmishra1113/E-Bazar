f83ec06692001799dff4df5d20b04792ea308fe5
saan-saurabh <49774071+saan-saurabh@users.noreply.github.com>
2021-03-15 01:15:17 +0530

    updated seller landing page with new php scripts
    dynamic fetching of data from databse achieved for seller information as well as the list of product listed by them.

--- templates/views/seller_landing_page.php
+++ templates/views/seller_landing_page.php
@@ -1,30 +1,45 @@
 <?php include "../../src/db_con.php" ?>
+<?php 
+$con= mysqli_connect("localhost", "id16099468_capstone", "Pd@123456789", "id16099468_ebazar")or die($mysqli_error($con));
+session_start();
+?>
 
 <?php include "../includes/header.php" ?>
 
 <?php $index="../../index.php"; $path="../../"; include "../includes/navbar.php" ?>
 
+
 <!--seller details  Start-->  
  <div class="container-fluid product_page">
     <div class="row">
-       <div class="col-lg-2 filter hide-on-small">
+       <div class="col-lg-2">
          
          <div class="card">
              <div class="card-header" style="display:flex; justify-content:space-between;">
-            <div>My Profile</div>
+            <h5>My Profile</h5>
             <button class="btn btn-warning btn-sm"><a href="#" data-toggle="modal" data-target="#seller_info_edit"><strong>Edit</strong></a></button>
             </div>
-            <div class="card body">
-              <h6 class="card-title text-secondary">SHOP NAME/SELLER NAME(from db)</h6>
-              <ul>
-                <li>Owner</li>
-                <li>Email</li>
-                <li>Phone</li>
-                <li>Adrees</li>
-                <li>city</li>
-                <li>state</li>
-                <li>pin</li>
-                </ul>
+            <div class="card body p-3">
+                <?php
+$email=$_SESSION['seller_email'];
+$query = "SELECT * FROM sellers WHERE seller_email='$email'";
+$result = mysqli_query($con, $query)or die($mysqli_error($con));
+$num = mysqli_num_rows($result);
+$row = mysqli_fetch_array($result);
+if($num >= 1)
+{
+    echo "<h6 class='card-title text-danger'style='text-transform:uppercase; font-weight:bold'>".$row['shop_name']."</h6><hr/>";
+    echo "<ul style='list-style-type:none;'>
+                <li><span class='text-primary'>Owner:</span>".$row['owner']."</li>
+                <li><span class='text-primary'>Email:</span>".$row['seller_email']."</li>
+                <li><span class='text-primary'>Contact:</span>".$row['seller_phone']."</li>
+                <li><span class='text-primary'>Address:</span>".$row['seller_address']."</li>
+                <li><span class='text-primary'>City:</span>".$row['seller_city']."</li>
+                <li><span class='text-primary'>State:</span>".$row['seller_state']."</li>
+                <li><span class='text-primary'>Pincode:</span>".$row['seller_pincode']."</li>
+                </ul>";
+}
+?>
             </div>
          </div>
 
@@ -36,11 +51,51 @@
                  <div><h5>Listed Items</h5></div>
                  <button class="btn btn-warning text-right"><a href="#" data-toggle="modal" data-target="#AddItems"><strong>Add Items</strong></a></button>
                  </div>
-            
-           
+        
+        
            <div class="card-body">
-         <p> all the listed item of this particular seller will be listed here from db</p>
+              
+             <!--Product Card Start-->
+             <div class="row">
+             <?php
+              //$category=$_REQUEST['product_key'];
+              $id=$_SESSION['seller_id'];
+              $query="SELECT * FROM products WHERE seller_id='$id'";
+              $result = mysqli_query($con, $query)or die($mysqli_error($con));
+              $num = mysqli_num_rows($result);
+              $row = mysqli_fetch_array($result);
+              if($num >= 1)
+              {
+               while($row=mysqli_fetch_array($result))
+                {
+                   $tempimg = $row['product_images'];
+                  $img = explode(",",$tempimg);
+                ?>
+              <div class="col-6 col-md-4 col-lg-3">
+               <div class="card">
+                 <a class="text-center" href="product_detail.php?product_key=<?php echo $row['product_id']; ?>"><img src="../../public/images/products/<?php echo $img[0]; ?>" class="img-fluid"></a>
+                 <div class="card-footer">
+                   <figcaption class="figure-caption brand">E-BAZAR</figcaption>
+                   <a href="product_detail.php?product_key=<?php echo $row['product_id']; ?>"><figcaption class="figure-caption title text-truncate"><?php echo $row['product_name']; ?></figcaption></a>
+                   <figcaption class="figure-caption color text-truncate"><?php echo $row['product_color'] ?></figcaption>
+                   <figcaption class="figure-caption rs">₹<?php $x=$row['product_price'];$y=$row['product_discount']; $money=($x-($x*$y)/100); echo $money; ?> <del>₹<?php echo $row['product_price']; ?></del> <span class="text-success"><?php echo $row['product_discount']; ?>% off</span></figcaption>
+                   <button class="btn btn-primary w-100 mt-1">Edit Details</button>
+                 </div>
+               </div>
+              </div>
+            <?php
+              }
+              }
+              else
+              {
+                  echo "<h5>No Product Listed yet!</h5>";
+              }
+            ?>
+            </div>
+             <!--Product Card End-->         
            </div>
+
+
 
            <!--Pagination Start-->
             <nav aria-label="Page navigation example">
@@ -123,18 +178,18 @@
         </div>
         <div class="modal-body justify-content-right">
           <div class="text-center pt-5 pb-5">
-              <form  action="#" method="POST">
+              <form  action="./seller_info_update_script.php" method="POST">
             <div class="form-group">
               <input type="text" name="shopname" class="form-control" placeholder="Shop Name/ Seller name">
             </div>
 
             <div class="form-group">
               <input type="text" name="owner" class="form-control"
-              placeholder="Owner">
+              placeholder="owner">
             </div>
             <div class="form-group">
               <input type="email" name="email" class="form-control"
-              placeholder="Email">
+              placeholder="Email" readonly>
             </div>
              <div class="form-group">
               <input type="contact" name="contact" class="form-control"
@@ -167,5 +222,5 @@
     </div>
   </div>
   <!-- modal end for seller info update ends-->
+  
 <?php include "../includes/footer.php" ?>
-
