cb990633b83f15b2fc6c3530c7647664fc466c46
saan-saurabh <49774071+saan-saurabh@users.noreply.github.com>
2021-03-19 15:39:48 +0530

    seller confirmation for deleting the product
    when seller will remove product this script will run for confirming the task

---
+++ templates/views/seller_product_delete_confirmation_script.php
@@ -0,0 +1,5 @@
+<?php
+$con= mysqli_connect("localhost", "id16099468_capstone", "Pd@123456789", "id16099468_ebazar")or die($mysqli_error($con));
+$id=$_REQUEST['id'];
+echo "<script type='text/javascript'>if(window.confirm('Sure! Want to Remove this item?')){window.location.href='seller_product_delete_script.php?id={$id}';}else{window.location.href='seller_product_details.php?product_key={$id}';}</script>";
+?>
