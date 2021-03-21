22bbb268a1b0002b06ea3d932302342993cf713e
PD1113 <prabhakarmishra1113@gmail.com>
2021-03-21 21:26:53 +0530

    Adding Database In Buy Product Page

---
+++ src/server/order.php
@@ -0,0 +1,44 @@
+<?php
+ include "../../src/db_con.php";
+ session_start();
+
+if(isset($_SESSION['user_id'])){
+    if(isset($_REQUEST['order'])){
+           $order_name=$order_number=$order_pin=$order_locality=$order_address=$order_town=$order_state=$address_type="";
+
+           $order_name=$_REQUEST['order_name'];
+           $order_number=$_REQUEST['order_number'];
+           $order_pin=$_REQUEST['order_pin'];
+           $order_locality=$_REQUEST['order_locality'];
+           $order_address=$_REQUEST['order_address'];
+           $order_town=$_REQUEST['order_town'];
+           $order_state=$_REQUEST['order_state'];
+           $address_type=$_REQUEST['address_type'];
+           $user_id=$_SESSION['user_id'];
+
+        if(empty($order_name)||empty($order_number)||empty($order_pin)||empty($order_locality)||empty($order_address)||empty($order_town)||empty($order_state)||empty($address_type)){
+            header("location:../../index.php");
+            $_SESSION['address_msg']="Fill All The Details";
+        }
+        else{
+            $query="INSERT INTO user_address(buyer_name,buyer_number,buyer_town,buyer_state,buyer_pin,buyer_locality,buyer_address,address_type,user_id) VALUES(?,?,?,?,?,?,?,?,?)";
+            $result=$con->prepare($query);
+            $result->execute(array($order_name,$order_number,$order_town,$order_state,$order_pin,$order_locality,$order_address,$address_type,$user_id));
+  
+            if($result)
+            {
+              header("location: ../../templates/views/buy_product.php");
+            }
+            else
+            {
+              header("location: ../../templates/views/buy_product.php");
+              $_SESSION['address_msg']="Server Error";
+            }
+        }
+    }
+ }
+ else{
+     header("location: ../../index.php");
+ }
+
+ ?>
--- templates/views/buy_product.php
+++ templates/views/buy_product.php
@@ -1,3 +1,4 @@
+<?php include "../../src/db_con.php"; session_start(); ?>
 <?php include "../includes/header.php" ?>
 
 <?php  $index="../../index.php"; $path="../../"; include "../includes/navbar.php" ?>
@@ -11,50 +12,61 @@
       <div class="card bg-white login_part mb-3">
         <div class="card-header bg-white">
           <h6 class="text-muted">LOGIN</h6>
-          <p><span class="name">Viraj</span>  <span class="text-muted number">+918360886874</span></p>
+          <p><span class="name"><?php echo $_SESSION['user_name']; ?></span>  <span class="text-muted number">+918360886874</span></p>
         </div>
        </div>
 
        <div class="address_part card mb-3">
-         <div class="card-body">  
-            <h6 class="card-title text-primary font-weight-bold">Add New Address</h6>
+       
+        <div class="card-body">
           <form>
+            <div class="form-check form-check-inline">
+                 <input class="form-check-input" type="radio" name="address_type" id="inlineRadio2" value="Work">
+                 <label class="form-check-label" for="inlineRadio2">Work (10 Delivery between  10 AM - 5 PM)</label>
+            </div>
+          </form>
+        </div>
+
+         <div class="card-body">
+            <h5><?php if(isset($_SESSION['address_msg'])){echo $_SESSION['address_msg'];} ?></h5>    
+            <h6 class="card-title text-primary font-weight-bold">Add New Address</h6>
+          <form action="../../src/server/order.php">
            <div class="form-row"> 
              <div class="form-group col-md-6">
-                 <input type="text" class="form-control" placeholder="Name">
+                 <input type="text" class="form-control" name="order_name" placeholder="Name">
              </div>
              <div class="form-group col-md-6">
-                 <input type="text" class="form-control" placeholder="10-digit mobile number">
+                 <input type="tel" class="form-control" name="order_number" placeholder="10-digit mobile number">
              </div>
              <div class="form-group col-md-6">
-                 <input type="text" class="form-control" placeholder="Pincode">
+                 <input type="text" class="form-control" name="order_pin" placeholder="Pincode">
              </div>
              <div class="form-group col-md-6">
-                 <input type="text" class="form-control" placeholder="Locality">
+                 <input type="text" class="form-control" name="order_locality" placeholder="Locality">
              </div>
              <div class="form-group col-md-12">
-                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Address(Area and Street)"></textarea>
+                 <textarea class="form-control" id="exampleFormControlTextarea1" name="order_address" rows="3" placeholder="Address(Area and Street)"></textarea>
              </div>
              <div class="form-group col-md-6">
-                 <input type="text" class="form-control" placeholder="City/District/Town">
+                 <input type="text" class="form-control" name="order_town" placeholder="City/District/Town">
              </div>
              <div class="form-group col-md-6">
-                 <input type="text" class="form-control" placeholder="State">
+                 <input type="text" class="form-control" name="order_state" placeholder="State">
              </div>
 
             <div class="form-group ml-3">
               <label class="text-muted">Address Type</label><br>  
               <div class="form-check form-check-inline">
-                 <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                 <input class="form-check-input" type="radio" name="address_type" id="inlineRadio1" value="Home">
                  <label class="form-check-label" for="inlineRadio1">Home (All day delivery)</label>
               </div>
              
               <div class="form-check form-check-inline">
-                 <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                 <input class="form-check-input" type="radio" name="address_type" id="inlineRadio2" value="Work">
                  <label class="form-check-label" for="inlineRadio2">Work (10 Delivery between  10 AM - 5 PM)</label>
               </div>
             </div> 
-             <button type="submit" class="btn btn-lg btn-danger" style="font-size: 14px;">SAVE AND DELIVER HERE</button>
+             <button type="submit" name="order" class="btn btn-lg btn-danger" style="font-size: 14px;">SAVE AND DELIVER HERE</button>
             </div>   
          </form>
         </div> 
@@ -91,3 +103,4 @@
 </section>
 
 <?php include "../includes/footer.php" ?>
+<?php unset($_SESSION['address_msg']) ?>
